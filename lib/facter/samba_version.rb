93cb78d653b0655ab5bb6a20bd8b5302104b559e
mergwyn <gary@theclarkhome.com>
2024-10-21 15:25:14 +0100

    Fix error in fact resolution

--- lib/facter/samba_version.rb
+++ lib/facter/samba_version.rb
@@ -1,18 +1,18 @@
 #!/usr/bin/ruby
 require 'facter'
 
-package = 'samba'
+# package = 'samba'
 
 
 distid = Facter.value(:operatingsystem)
 
 case distid
     when /RedHatEnterprise|CentOS|Fedora|RHEL/
-        if  FileTest.exists?("/usr/bin/yum")
+        if Facter::Util::Resolution.which('yum')
             version = Facter::Util::Resolution.exec('/usr/bin/yum info samba | sed \'s/Version *: \([0-9\.]\+\)/\1/gp;d\' | head -n 1')
         end
     when /Ubuntu|Debian/
-        if  FileTest.exists?("/usr/bin/apt-cache")
+        if Facter::Util::Resolution.which('apt-cache')
             version = Facter::Util::Resolution.exec('apt-cache show samba | sed \'s/Version:.*:\([0-9\.]\+\).*/\1/gp;d\' | head -n 1')
         end
     when 'Archlinux'
